05ac5e5dd75bf7e97282b9ff6ca863ea8f61a179
SaimK01 <saimkhan6410@gmail.com>
2024-02-21 18:09:17 +0500

    learn oop

---
+++ oop.rb
@@ -0,0 +1,13 @@
+class Student
+@first_name
+@second_name
+    def to_s(first_name, second_name)
+        puts "First name is #{first_name} second name is #{second_name}"
+    end
+
+end
+
+
+object= Student.new
+object.to_s("Saim","Khan")
+# haha this as the mistake, yahhh :) ab saim desa
